c52f3a628fde784387e8d1b269d463f81f6cfcb3
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2022-04-29 16:42:42 -0700

    Add debug information

--- http/src/main/resources/init-resources/msdsvmcontent/ms_dsvm_script_v5_cat.sh
+++ http/src/main/resources/init-resources/msdsvmcontent/ms_dsvm_script_v5_cat.sh
@@ -69,7 +69,11 @@ RELAY_CONNECTIONSTRING="Endpoint=sb://${RELAY_NAME}.servicebus.windows.net/;Shar
 
 # Start Jupyter server with custom parameters
 
-sudo runuser -l $VM_JUP_USER -c "/anaconda/bin/jupyter server --ServerApp.certfile=$SERVER_APP_CERTFILE --ServerApp.keyfile=$SERVER_APP_KEYFILE --ServerApp.port=$SERVER_APP_PORT --ServerApp.token=$SERVER_APP_TOKEN --ServerApp.ip=$SERVER_APP_IP --ServerApp.base_url=$SERVER_APP_BASE_URL --ServerApp.websocket_url=$SERVER_APP_WEBSOCKET_URL --ServerApp.allow_origin=$SERVER_APP_ALLOW_ORIGIN autoreload" >/dev/null 2>&1&
+JUPYTER_RUN_COMMAND="/anaconda/bin/jupyter server --ServerApp.certfile=$SERVER_APP_CERTFILE --ServerApp.keyfile=$SERVER_APP_KEYFILE --ServerApp.port=$SERVER_APP_PORT --ServerApp.token=$SERVER_APP_TOKEN --ServerApp.ip=$SERVER_APP_IP --ServerApp.base_url=$SERVER_APP_BASE_URL --ServerApp.websocket_url=$SERVER_APP_WEBSOCKET_URL --ServerApp.allow_origin=$SERVER_APP_ALLOW_ORIGIN autoreload"
+echo "RUN COMMAND\n"
+echo $JUPYTER_RUN_COMMAND
+sudo runuser -l $VM_JUP_USER -c $JUPYTER_RUN_COMMAND >/dev/null 2>&1&
+echo "END RUN COMMAND\n"
 
 # Store Jupyter Server parameters for reboot process
 
